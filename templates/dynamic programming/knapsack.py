d89414b5520b96a33f65d1de87d0653c3187b4a1
manish17 <35730135+iammanish17@users.noreply.github.com>
2020-08-13 20:02:49 +0530

    Update knapsack.py

--- templates/dynamic programming/knapsack.py
+++ templates/dynamic programming/knapsack.py
@@ -1,11 +1,8 @@
 def knapsack(limit, values, weights):
     """Returns the maximum value that can be reached using given weights"""
     n = len(weights)
-    dp = [[0]*(n+1) for i in range(limit+1)]
-    for i in range(1, limit+1):
-        for j in range(1, n+1):
-            if i < weights[j-1]:
-                dp[i][j] = dp[i][j-1]
-            else:
-                dp[i][j] = max(dp[i-weights[j-1]][j-1] + values[j-1], dp[i][j-1])
-    return dp[limit][n]
+    dp = [0]*(limit+1)
+    for i in range(n):
+        for j in range(limit, weights[i]-1, -1):
+            dp[j] = max(dp[j], values[i] + dp[j - weights[i]])
+    return dp[-1]
