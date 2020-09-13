4e9db55404feead80ccb9eac6e7a4ccd86440085
manish17 <35730135+iammanish17@users.noreply.github.com>
2020-09-13 15:51:28 +0530

    Create sieve.py

---
+++ templates/algebra/sieve.py
@@ -0,0 +1,11 @@
+def SieveOfEratosthenes(limit=10**6):
+    """Returns all primes not greater than limit."""
+    isPrime = [True]*(limit+1)
+    isPrime[0] = isPrime[1] = False
+    primes = []
+    for i in range(2, limit+1):
+        if not isPrime[i]:continue
+        primes += [i]
+        for j in range(i*i, limit+1, i):
+            isPrime[j] = False
+    return primes
