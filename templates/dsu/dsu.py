1a23cb5e927d94eab097942432b0c4cb53f36982
manish17 <35730135+iammanish17@users.noreply.github.com>
2020-09-14 10:57:08 +0530

    Create dsu.py

---
+++ templates/dsu/dsu.py
@@ -0,0 +1,33 @@
+class DisjointSetUnion:
+    def __init__(self, n):
+        self.parent = [*range(n+1)]
+        self.size = [1]*(n+1)
+        self.min, self.max = [*range(n+1)], [*range(n+1)]
+        self.count = n
+
+    def get(self, a):
+        """Returns the identifier (parent) of the set to which a belongs to!"""
+        if self.parent[a] == a:
+            return a
+        x = a
+        while a != self.parent[a]:
+            a = self.parent[a]
+        while x != self.parent[x]:
+            self.parent[x], x = a, self.parent[x]
+        return a
+
+    def union(self, a, b):
+        """Join two sets that contain a and b!"""
+        a, b = self.get(a), self.get(b)
+        if a != b:
+            if self.size[a] > self.size[b]:
+                a, b = b, a
+            self.parent[a] = b
+            self.size[b] += self.size[a]
+            self.min[b] = min(self.min[a], self.min[b])
+            self.max[b] = max(self.max[a], self.max[b])
+            self.count -= 1
+
+    def count_sets(self):
+        """Returns the number of disjoint sets!"""
+        return self.count
