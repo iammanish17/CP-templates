d1b09fb7988affa5efa5015c54c3f2ecd27e7bfe
Manish Kundu <35730135+iammanish17@users.noreply.github.com>
2021-01-07 12:31:05 +0530

    Update subsetsum.py

--- templates/dynamic programming/subsetsum.py
+++ templates/dynamic programming/subsetsum.py
@@ -5,3 +5,16 @@ def check_subset_sum(a, n):
         for j in range(n, a[i] - 1, -1):
             dp[j] = True if j == a[i] else dp[j] or dp[j - a[i]]
     return dp[-1]
+
+def get_subset_with_sum(a, n):
+    """Returns a subset of a with sum equal to n (if exists)."""
+    dp = [[]] * (n + 1)
+    for i in range(len(a)):
+        for j in range(n, a[i] - 1, -1):
+            if j == a[i]:
+                dp[j] = [a[i]]
+            elif dp[j]:
+                continue
+            elif dp[j - a[i]]:
+                dp[j] = dp[j - a[i]] + [a[i]]
+    return dp[-1]
